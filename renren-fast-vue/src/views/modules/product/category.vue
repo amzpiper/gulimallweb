87b74bd9991ca0f5d5e824b747f9594131e2342f
郭宇航 <1131717341@qq.com>
2021-04-12 20:45:06 +0800

    分类维护

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,46 +1,120 @@
 <template>
-    <div>
-        123
-    </div>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+    ></el-tree>
+  </div>
 </template>
 
 <script>
-  export default {
-    components: {
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1",
+                },
+              ],
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1",
+                },
+              ],
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
     },
-    props: {
+    // 获取菜单列表
+    getMenus() {
+      this.dataListLoading = true;
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("成功获取到菜单数据：" + data);
+        // if (data && data.code === 0) {
+        //   this.dataList = data.page.list;
+        //   this.totalPage = data.page.totalCount;
+        // } else {
+        //   this.dataList = [];
+        //   this.totalPage = 0;
+        // }
+        // this.dataListLoading = false;
+      });
     },
-    data () {
-      return {
-      }
-    },
-    computed: {
-    },
-    watch: {
-    },
-    methods: {
-    },
-    created () {
-    },
-    mounted () {
-    },
-    beforeCreate () {
-    },
-    beforeMount () {
-    },
-    beforeUpdate () {
-    },
-    updated () {
-    },
-    beforeDestroy () {
-    },
-    destroyed () {
-    },
-    activated () {
-        // 如果页面有keep-alive缓存功能，这个函数会触发
-    }
-  }
+  },
+  created() {
+    this.getMenus();
+  },
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {
+    // 如果页面有keep-alive缓存功能，这个函数会触发
+  },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
