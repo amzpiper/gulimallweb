847107063bc90e41276611cc47cb9dd9d31eb4ed
郭宇航 <1131717341@qq.com>
2021-04-12 21:04:01 +0800

    修改路由到/api

--- renren-fast-vue/static/config/index-qa.js
+++ renren-fast-vue/static/config/index-qa.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:88';
+  window.SITE_CONFIG['baseUrl'] = 'http://39.106.63.189:88';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
