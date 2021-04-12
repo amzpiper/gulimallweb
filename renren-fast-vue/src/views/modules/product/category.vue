575c7cd0972f633b1e3ca5cbd908a9d1bf403f96
郭宇航 <1131717341@qq.com>
2021-04-12 20:28:16 +0800

    三级分类

---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    props: {
+    },
+    data () {
+      return {
+      }
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+    },
+    created () {
+    },
+    mounted () {
+    },
+    beforeCreate () {
+    },
+    beforeMount () {
+    },
+    beforeUpdate () {
+    },
+    updated () {
+    },
+    beforeDestroy () {
+    },
+    destroyed () {
+    },
+    activated () {
+        // 如果页面有keep-alive缓存功能，这个函数会触发
+    }
+  }
+</script>
+<style lang="">
+    
+</style>
